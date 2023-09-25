54f496cc1ae7cca20dcb31f027bae2a7ca032f20
JL34 <johnny2791@seznam.cz>
2023-09-25 17:01:39 +0200

    hw5 random changes to figure it out

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -25,10 +25,10 @@ docs: https://developers.google.com/youtube/v3/docs/channels/list
 
 сервис для быстрого получения id канала: https://commentpicker.com/youtube-channel-id.php
 '''
-# channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
-channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
+channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
+# channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
 channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-printj(channel)
+#printj(channel)
 
 
 '''
@@ -39,7 +39,7 @@ playlists = youtube.playlists().list(channelId=channel_id,
                                      part='contentDetails,snippet',
                                      maxResults=50,
                                      ).execute()
-# printj(playlists)
+printj(playlists)
 for playlist in playlists['items']:
     print(playlist)
     print()
@@ -53,12 +53,14 @@ docs: https://developers.google.com/youtube/v3/docs/playlistItems/list
 https://www.youtube.com/playlist?list=PLH-XmS0lSi_zdhYvcwUfv0N88LQRt6UZn
 или из ответа API: см. playlists выше
 '''
-playlist_id = 'PLH-XmS0lSi_zdhYvcwUfv0N88LQRt6UZn'
+# playlist_id = 'PLH-XmS0lSi_zdhYvcwUfv0N88LQRt6UZn'
+playlist_id = 'PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw'
 playlist_videos = youtube.playlistItems().list(playlistId=playlist_id,
                                                part='contentDetails',
                                                maxResults=50,
                                                ).execute()
-# printj(playlist_videos)
+
+printj(playlist_videos)
 
 # получить все id видеороликов из плейлиста
 video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
@@ -78,7 +80,7 @@ for video in video_response['items']:
     # YouTube video duration is in ISO 8601 format
     iso_8601_duration = video['contentDetails']['duration']
     duration = isodate.parse_duration(iso_8601_duration)
-    print(duration)
+    # print(duration)
 
 
 '''
@@ -90,8 +92,8 @@ video_id = 'gaoc9MPZ4bw'
 video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                        id=video_id
                                        ).execute()
-printj(video_response)
-video_title: str = video_response['items'][0]['snippet']['title']
-view_count: int = video_response['items'][0]['statistics']['viewCount']
+# printj(video_response)
+# video_title: str = video_response['items'][0]['snippet']['title']
+# view_count: int = video_response['items'][0]['statistics']['viewCount']
 like_count: int = video_response['items'][0]['statistics']['likeCount']
-comment_count: int = video_response['items'][0]['statistics']['commentCount']
+# comment_count: int = video_response['items'][0]['statistics']['commentCount']
